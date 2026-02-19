838e7e24b1211423709104c3477a1b44195c34d7
M Dhanush <mdhanush11923@gmail.com>
2026-02-19 19:35:41 +0530

    lesson completed

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
+import { BrowserRouter } from "react-router";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root"),
 );
 
 // If you want to start measuring performance in your app, pass a function
